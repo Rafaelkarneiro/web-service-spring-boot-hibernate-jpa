c1e282aaffd7397cd56cc67c529b989a40b1747e
Rafael Campanin Carneiro <rafaelcampanincarneiro@gmail.com>
2023-12-04 15:23:31 -0300

    Product

---
+++ src/main/java/workshop/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import workshop.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
